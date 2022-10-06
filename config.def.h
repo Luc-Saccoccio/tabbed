dfac19e1e23e6cf8e36b322263c1e8e459c688f1
Luc-Saccoccio <lucsaccoccio@disroot.org>
2022-10-06 18:28:22 +0200

    Updating to tabbed v0.7

--- config.def.h
+++ config.def.h
@@ -6,10 +6,14 @@ static const char* normbgcolor  = "#222222";
 static const char* normfgcolor  = "#cccccc";
 static const char* selbgcolor   = "#555555";
 static const char* selfgcolor   = "#ffffff";
+static const char* urgbgcolor   = "#111111";
+static const char* urgfgcolor   = "#cc0000";
 static const char before[]      = "<";
 static const char after[]       = ">";
+static const char titletrim[]   = "...";
 static const int  tabwidth      = 200;
 static const Bool foreground    = True;
+static       Bool urgentswitch  = False;
 
 /*
  * Where to place a new tab when it is opened. When npisrelative is True,
@@ -20,15 +24,17 @@ static int  newposition   = 0;
 static Bool npisrelative  = False;
 
 #define SETPROP(p) { \
-	.v = (char *[]){ "/bin/sh", "-c", \
-		"prop=\"`xwininfo -children -id $1 | grep '^     0x' | sed -e's@^ *\\(0x[0-9a-f]*\\) \"\\([^\"]*\\)\".*@\\1 \\2@' | xargs -0 printf %b | dmenu -l 10`\" &&" \
-		"xprop -id $1 -f $0 8s -set $0 \"$prop\"", \
-		p, winid, NULL \
-	} \
+        .v = (char *[]){ "/bin/sh", "-c", \
+                "prop=\"`xwininfo -children -id $1 | grep '^     0x' |" \
+                "sed -e's@^ *\\(0x[0-9a-f]*\\) \"\\([^\"]*\\)\".*@\\1 \\2@' |" \
+                "xargs -0 printf %b | dmenu -l 10 -w $1`\" &&" \
+                "xprop -id $1 -f $0 8s -set $0 \"$prop\"", \
+                p, winid, NULL \
+        } \
 }
 
 #define MODKEY ControlMask
-static Key keys[] = { \
+static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY|ShiftMask,             XK_Return, focusonce,      { 0 } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          SETPROP("_TABBED_SELECT_TAB") },
@@ -52,6 +58,9 @@ static Key keys[] = { \
 	{ MODKEY,                       XK_0,      move,           { .i = 9 } },
 
 	{ MODKEY,                       XK_w,      killclient,     { 0 } },
+
+	{ MODKEY,               XK_u,      focusurgent, { 0 } },
+	{ MODKEY|ShiftMask,     XK_u,      toggle,      { .v = (void*) &urgentswitch } },
 
 	{ 0,                            XK_F11,    fullscreen,     { 0 } },
 };
